0ee037e0a89cc4e368ec0ff5b581927c1bacd297
rbquick <quickrb@gmail.com>
2023-09-29 05:54:16 -0400

    Initial Commit

---
+++ StoreShopping/StoreShoppingApp.swift
@@ -0,0 +1,17 @@
+//
+//  StoreShoppingApp.swift
+//  StoreShopping
+//
+//  Created by Brian Quick on 2023-09-29.
+//
+
+import SwiftUI
+
+@main
+struct StoreShoppingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
